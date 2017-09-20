a7c82d843b7477b94f0f719af4afb92371047cf6
aucontraire <ocontreras.sf@gmail.com>
2017-09-20 09:16:46 -0700

    Add allowed hosts

--- todizzle/settings.py
+++ todizzle/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'u4p!ky#dx0$!_a$hzl65t55ilngo@1!n^3)yz_+y0qm!k0n4w3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".elasticbeanstalk.com"]
 
 
 # Application definition
