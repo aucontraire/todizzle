1e6b95042c59f3d81cfcd61a1036900b0a3a485d
aucontraire <ocontreras.sf@gmail.com>
2017-09-21 15:18:38 -0700

    Add datetimepicker widget to due date

--- todos/views.py
+++ todos/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 from todos.models import Item
 
 from django.utils import timezone
+import datetime
 
 def home_page(request):
     if request.method == 'POST':
@@ -14,6 +15,10 @@ def home_page(request):
 
 def item_page(request, item_pk):
     item = Item.objects.get(pk=item_pk)
+    
+    if request.method == 'POST':
+        item.due_date = datetime.datetime.strptime(request.POST['due_date'], '%m/%d/%Y %I:%M %p') # (10/29/2015 12:00 AM) 
+        item.save()
     
     return render(request, 'item.html', { 'item': item })
     
@@ -34,3 +39,4 @@ def item_archive(request, item_pk):
 def item_delete(request, item_pk):
     item = Item.objects.get(pk=item_pk)
     item.delete()
+
